fc9d3aeb0e5390f040facf27876445f9df5ccbcf
IKRAM Hafsa <hafsa20030731@gmail.com>
2024-08-08 20:19:05 +0200

    Authentification

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Salaire</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:700,600" rel="stylesheet" type="text/css" />
+    @vite(['resources/css/auth.css'])
+</head>
+<body>
+
+<form method="post" action="{{route('handleLogin')}}">
+    @csrf
+    @method('POST')
+
+    <div class="box">
+        <h1>Espace de connexion</h1>
+        @if (Session::get('error_msg'))
+            <b style="font-size: 10px; color:rgb(185,81,81)">{{Session::get('error_msg')}}</b>
+        @endif
+
+        <input type="email" name="email" class="email" placeholder="Email" required />
+
+        <input type="password" name="password" class="email" placeholder="Mot de passe" required />
+
+        <div class="btn-container">
+            <button type="submit">Connexion</button>
+        </div>
+    </div>
+</form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[AuthController::class,'login'])->name('login');
+Route::post('/',[AuthController::class,'handleLogin'])->name('handleLogin');
+
+//Route sécurisé
+
+Route::get('dashboard',[AppController::class,'index'])->name('dashboard');
