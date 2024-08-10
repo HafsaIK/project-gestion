0852693498e4b56c4331d5386fd9e4bf8082d328
IKRAM Hafsa <hafsa20030731@gmail.com>
2024-08-10 19:50:54 +0200

    Afficher les statistiques réels sur le Dashboard

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Departement;
+use App\Models\Employer;
+use App\Models\User;
+use PHPUnit\Framework\Constraint\Count;
 
 class AppController extends Controller
 {
     public function index(){
-        return view('auth.dashboard');
+        $totalDepartements = Departement::all()->count();
+        $totalEmployers = Employer::all()->count();
+        $totalAdministrateurs = User::all()->count();
+        return view('auth.dashboard',compact('totalDepartements','totalEmployers','totalAdministrateurs'));
     }
 }
--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<h1 class="app-page-title">Overview</h1>
+<h1 class="app-page-title">Dashboard</h1>
 			    
 <div class="app-card alert alert-dismissible shadow-sm mb-4 border-left-decoration" role="alert">
     <div class="inner">
@@ -31,12 +31,8 @@
     <div class="col-6 col-lg-3">
         <div class="app-card app-card-stat shadow-sm h-100">
             <div class="app-card-body p-3 p-lg-4">
-                <h4 class="stats-type mb-1">Total Sales</h4>
-                <div class="stats-figure">$12,628</div>
-                <div class="stats-meta text-success">
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-                    </svg> 20%</div>
+                <h4 class="stats-type mb-1">Total Départements</h4>
+                <div class="stats-figure">{{$totalDepartements}}</div>
             </div><!--//app-card-body-->
             <a class="app-card-link-mask" href="#"></a>
         </div><!--//app-card-->
@@ -45,12 +41,8 @@
     <div class="col-6 col-lg-3">
         <div class="app-card app-card-stat shadow-sm h-100">
             <div class="app-card-body p-3 p-lg-4">
-                <h4 class="stats-type mb-1">Expenses</h4>
-                <div class="stats-figure">$2,250</div>
-                <div class="stats-meta text-success">
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-                    </svg> 5% </div>
+                <h4 class="stats-type mb-1">Total Employers</h4>
+                <div class="stats-figure">{{$totalEmployers}}</div>
             </div><!--//app-card-body-->
             <a class="app-card-link-mask" href="#"></a>
         </div><!--//app-card-->
@@ -58,10 +50,8 @@
     <div class="col-6 col-lg-3">
         <div class="app-card app-card-stat shadow-sm h-100">
             <div class="app-card-body p-3 p-lg-4">
-                <h4 class="stats-type mb-1">Projects</h4>
-                <div class="stats-figure">23</div>
-                <div class="stats-meta">
-                    Open</div>
+                <h4 class="stats-type mb-1">Total Administrateurs</h4>
+                <div class="stats-figure">{{$totalAdministrateurs}}</div>
             </div><!--//app-card-body-->
             <a class="app-card-link-mask" href="#"></a>
         </div><!--//app-card-->
@@ -69,9 +59,8 @@
     <div class="col-6 col-lg-3">
         <div class="app-card app-card-stat shadow-sm h-100">
             <div class="app-card-body p-3 p-lg-4">
-                <h4 class="stats-type mb-1">Invoices</h4>
-                <div class="stats-figure">6</div>
-                <div class="stats-meta">New</div>
+                <h4 class="stats-type mb-1">Retard de paiement</h4>
+                <div class="stats-figure">0</div>
             </div><!--//app-card-body-->
             <a class="app-card-link-mask" href="#"></a>
         </div><!--//app-card-->
