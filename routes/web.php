a5c4e449644c58fc639c710d239b78ac9b3ba795
IKRAM Hafsa <hafsa20030731@gmail.com>
2024-08-12 00:00:11 +0200

    Ecran liste et Ajout des employers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[AuthController::class,'login'])->name('login');
@@ -9,4 +10,13 @@ Route::post('/',[AuthController::class,'handleLogin'])->name('handleLogin');
 
 //Route sécurisé
 
-Route::get('dashboard',[AppController::class,'index'])->name('dashboard');
+Route::middleware('auth')->group(function(){
+    Route::get('dashboard',[AppController::class,'index'])->name('dashboard'); 
+
+    Route::prefix('employers')->group(function(){
+        Route::get('/',[EmployerController::class,'index'])->name('employer.index') ;
+        Route::get('/create',[EmployerController::class,'create'])->name('employer.create') ;
+        Route::get('/edit/{employer}',[EmployerController::class,'edit'])->name('employer.edit') ;
+
+    });
+});
