47919396e314b9429e836e6c12ef35e59e877a3a
IKRAM Hafsa <hafsa20030731@gmail.com>
2024-08-21 22:31:08 +0200

    Rappel de paiement sur le dashboard

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Departement;
 use App\Models\Employer;
 use App\Models\User;
+use Carbon\Carbon;
 use PHPUnit\Framework\Constraint\Count;
 
 class AppController extends Controller
@@ -17,10 +18,38 @@ class AppController extends Controller
         $totalAdministrateurs = User::all()->count();
         
 
-        $appName= Configuration::where('type','APP_NAME')->first();
+        //$appName= Configuration::where('type','APP_NAME')->first();
 
+        $defaultPaymentDateQuery = null;
+        $paymentNotification = "";
+
+        $currentDate = Carbon::now()->day;
+
+        $defaultPaymentDateQuery= Configuration::where('type','PAYMENT_DATE')->first();
+        
+        if ($defaultPaymentDateQuery) {
+            $defaultPaymentDate = $defaultPaymentDateQuery->value;
+            $convertPaymentDate = intval($defaultPaymentDate);
+
+            if ($currentDate === $convertPaymentDate) {
+
+                $paymentNotification = "Le paiement doit avoir lieu aujourd'hui, le " . $defaultPaymentDate . " de ce mois";
+            
+            } elseif ($currentDate < $convertPaymentDate) {
+            
+                $paymentNotification = "Le paiement doit avoir lieu le " . $defaultPaymentDate . " de ce mois";
+            
+            } else {
+            
+                $nextMonth = Carbon::now()->addMonth();
+                $nextMonthName = $nextMonth->format('F');
+                
+                $paymentNotification = "Le paiement doit avoir lieu le " . $defaultPaymentDate . " du mois de " . $nextMonthName;
+            
+            }
+        }
         
 
-        return view('auth.dashboard',compact('totalDepartements','totalEmployers','totalAdministrateurs'));
+        return view('auth.dashboard',compact('totalDepartements','totalEmployers','totalAdministrateurs','paymentNotification'));
     }
 }
